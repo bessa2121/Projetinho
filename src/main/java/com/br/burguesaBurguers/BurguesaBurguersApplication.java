9b35eae91a985d86519035bb2457ed695e05632a
Davi Tavares <dxrktavares3@gmail.com>
2026-01-13 19:28:48 -0300

    Adiciona primeiro modelo spring do projeto de BurguesasBurguer

---
+++ src/main/java/com/br/burguesaBurguers/BurguesaBurguersApplication.java
@@ -0,0 +1,13 @@
+package com.br.burguesaBurguers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguesaBurguersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurguesaBurguersApplication.class, args);
+	}
+
+}
